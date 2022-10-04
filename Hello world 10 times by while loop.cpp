7073818a4959a76745bedb6654c8c72c27e4cfb2
salimmwamabishi <113597580+salimmwamabishi@users.noreply.github.com>
2022-10-04 12:28:27 +0300

    Add files via upload

---
+++ Hello world 10 times by while loop.cpp
@@ -0,0 +1,13 @@
+//programe to print hello world 10 times
+#include<iostream>
+using namespace std;
+int main()
+{
+ int count=1;
+ while(count<=10)
+ {
+  cout<<"Hello world"<<endl;
+  count=count+1;
+ }
+ return 0;
+}
