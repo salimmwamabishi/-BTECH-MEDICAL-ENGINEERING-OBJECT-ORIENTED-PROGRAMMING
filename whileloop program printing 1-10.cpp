7c66d61a64603f2818d5ce354f79aa64747d891b
salimmwamabishi <113597580+salimmwamabishi@users.noreply.github.com>
2022-10-04 12:42:00 +0300

    Add files via upload

---
+++ whileloop program printing 1-10.cpp
@@ -0,0 +1,13 @@
+//programe to print number1-10
+#include<iostream>
+using namespace std;
+int main()
+{
+ int count=1;
+ //while loop from 1-10
+ while(count<=10)
+ {cout<<count<<" ";
+++count;
+ }
+ return 0;
+}
